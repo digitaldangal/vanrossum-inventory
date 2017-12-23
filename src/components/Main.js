125d719a1d04a3163a68132a630eb172cac43f45
Pieter Stokkink <pieterstokkink@gmail.com>
2017-12-23 18:44:15 +0100

    adds first entry component

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { ipcRenderer, remote } from 'electron'
+import { Redirect } from 'react-router-dom'
+
+import WatchFolder from './WatchFolder'
+
+class Main extends Component {
+    redirect = () => {
+        if (this.props.ready) {
+            return <Redirect to="/scan" />
+        } else {
+            return <div/>
+        }
+    }
+
+    render () {
+        return (
+            <div className="app">
+                <WatchFolder/>
+                {this.props.children}
+                {this.redirect()}
+            </div>
+        )
+    }
+}
+
+function mapStateToThis (state) {
+    return {
+        ready: state.path
+    }
+}
+
+export default connect(mapStateToThis)(Main)
