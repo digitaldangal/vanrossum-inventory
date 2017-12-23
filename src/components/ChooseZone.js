b78098a3b61b5514f29b078416c8ce6011160e43
Pieter Stokkink <pieterstokkink@gmail.com>
2017-12-23 18:44:30 +0100

    adds component to choose zone

---
+++ src/components/ChooseZone.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addZone } from '../actions'
+import { ipcRenderer } from 'electron'
+import { withRouter } from 'react-router'
+
+class ChooseZone extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {zone: ''}
+    
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange(event) {
+        this.setState({zone: event.target.value})
+    }
+    
+    handleSubmit(event) {
+        event.preventDefault()
+        if (this.state.zone > 9999 || this.state.zone < 1) {
+            alert('zonenummer ongeldig')
+        } else if (this.props.zones.includes(Number(this.state.zone))) {
+            alert(`zone ${this.state.zone} bestaat al!`)
+        } else {
+            ipcRenderer.send('add-zone', this.state.zone, this.props.file)
+            this.props.setZone(this.state.zone)
+        }
+    }
+
+    render () {
+        return (
+            <div className="kieszone">
+                <h1>Kiez Zone</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="number" value={this.state.zone} onChange={this.handleChange}/>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+function mapStateToThis (state) {
+    return {
+        zones: state.zones,
+        file: state.file
+    }
+}
+
+function mapDispatchToThis (dispatch) {
+    return {
+        setZone: (zone) => {
+            dispatch(addZone(Number(zone)))
+        }
+    }
+}
+
+export default withRouter(
+    connect(mapStateToThis, mapDispatchToThis)(ChooseZone)
+)
