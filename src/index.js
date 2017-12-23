480b00efd79461819c3ad22e69284971b232cecf
Pieter Stokkink <pieterstokkink@gmail.com>
2017-12-23 18:43:15 +0100

    adds main index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import store from './reducers'
+
+import Main from './components/Main'
+import Zone from './components/Zone'
+import Scan from './components/Scan'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+        <div className="app">
+            <Switch>
+                <Route path="/scan" component={Scan} />
+                <Route path="/" component={Main} />
+            </Switch>
+        </div>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
