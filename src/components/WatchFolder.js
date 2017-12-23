c473d385589cc97ec58c6bd90907d413fd115906
Pieter Stokkink <pieterstokkink@gmail.com>
2017-12-23 18:44:40 +0100

    adds component to choose folder to watch

---
+++ src/components/WatchFolder.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { ipcRenderer, remote } from 'electron'
+import { choosePath } from '../actions'
+import { Redirect } from 'react-router-dom'
+
+class WatchFolder extends Component {
+    clickHandler = () => {
+        remote.dialog.showOpenDialog({
+            properties: ['openDirectory']
+        }, (path) => {
+            if (path) {
+                ipcRenderer.send('watch-folder', path)
+                ipcRenderer.on('watcher-ready', () => {
+                    this.props.selectFolder(path)
+                })
+            }else {
+                console.log("No path selected")
+            }
+        })
+    }
+
+    show = () => {
+        if (this.props.path) {
+            return (
+                <div>
+                    <p>Bestanden die in {this.props.path} terechtkomen zullen worden getransformeerd</p>
+                    <button onClick={this.clickHandler.bind(this)}>Selecteer andere locatie</button>
+                    {this.props.children}
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>Selecteer map waar bestanden terecht komen:</p>
+                    <button onClick={this.clickHandler}>Kies map</button>
+                    {this.props.children}
+                </div>
+            )
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                { this.show() }
+            </div>
+        )
+    }
+}
+
+function mapStateToThis (state) {
+    return {
+        path: state.path
+    }
+}
+
+function mapDispatchToThis (dispatch) {
+    return {
+        selectFolder: (path) => {
+            dispatch(choosePath(path[0]))
+        }
+    }
+}
+
+export default connect(mapStateToThis, mapDispatchToThis)(WatchFolder)
